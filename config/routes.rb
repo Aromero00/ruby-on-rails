4177dcc991534449679be4c1057364a1b95d03cd
Antonio romero <aromero@atheva.com.br>
2023-05-09 13:57:25 -0300

    autenticação com JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :auths, only: [:create]
   resources :kiinds
   resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
