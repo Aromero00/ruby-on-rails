916f4b4d7a3dc4205b8850e408af500a36bc3a58
Antonio romero <aromero@atheva.com.br>
2023-05-09 10:11:33 -0300

    autenticação com token

--- app/controllers/kiinds_controller.rb
+++ app/controllers/kiinds_controller.rb
@@ -3,11 +3,17 @@ class KiindsController < ApplicationController
 #include ActionController::HttpAuthentication::Basic::ControllerMethods 
  #http_basic_authenticate_with name: "antonio", password: "123456"
 
- include ActionController::HttpAuthentication::Digest::ControllerMethods
-# jack:application:secret
-  USERS = {"antonio" => OpenSSL::Digest::MD5.hexdigest(["antonio","application","secret"].join(":"))}  #ha1 digest password
+# include ActionController::HttpAuthentication::Digest::ControllerMethods
+ #USERS = {"antonio" => OpenSSL::Digest::MD5.hexdigest(["antonio","application","secret"].join(":"))}  #ha1 digest password
   # GET /kiinds
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  TOKEN = "secret123"
+
   before_action :authenticate
+  
+  
   def index
     @kiinds = Kiind.all
 
@@ -55,9 +61,19 @@ class KiindsController < ApplicationController
       params.require(:kiind).permit(:description)
     end
 
+    #def authenticate
+      #authenticate_or_request_with_http_digest("application") do |username|
+      #  USERS[username]
+      #end
+    #end
+
     def authenticate
-      authenticate_or_request_with_http_digest("application") do |username|
-        USERS[username]
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
       end
     end
+
+
 end
